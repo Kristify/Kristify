be2949d3fe0ca054f0b3a5642723f6a9283de128
sovnya <enp7s1@tutanota.com>
2022-12-23 12:29:21 +0100

    feat: Add lua-language-server annotations for utils file
    This commit provides in-line documentation for the utilities file

--- src/utils.lua
+++ src/utils.lua
@@ -1,9 +1,18 @@
 local utils = {}
 
+---Checks if `str` ends with `ending`
+---This is a simple utility function so we don't have a lot of repetitive code
+---@param str string
+---@param ending string
+---@return boolean
 function utils.endsWith(str, ending)
   return ending == "" or str:sub(- #ending) == ending
 end
 
+---Checks if `products` include a certain name
+---@param products table
+---@param metaname string
+---@return boolean
 function utils.productsIncludes(products, metaname)
   for _, product in ipairs(products) do
     if product.metaname == metaname then
@@ -14,6 +23,10 @@ function utils.productsIncludes(products, metaname)
   return false
 end
 
+---Gets a product by name
+---@param products table
+---@param metaname string
+---@return table|false
 function utils.getProduct(products, metaname)
   for _, product in ipairs(products) do
     if product.metaname == metaname then
