a08db813d5a84bb9736c5cefc8a8e3630f51c1ad
Erlend <49862976+Erb3@users.noreply.github.com>
2022-12-23 16:01:04 +0100

    Add messages to config.example

--- data/config.example.lua
+++ data/config.example.lua
@@ -4,5 +4,12 @@ return {
   storage = {
     ""
   },
-  self = ""
+  self = "",
+  messages = {
+    noMetaname      = "message=No metaname found! Refunding.",
+    nonexistantItem = "message=The item you requested is not available for purchase",
+    notEnogthMoney  = "message=Insufficient amount of krist sent.",
+    notEnogthStock  = "message=We don't have that much stock!",
+    change          = "message=Here is your change! Thanks for using our shop."
+  }
 }
