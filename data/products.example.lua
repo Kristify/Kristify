52c8a7df67ac87e3bfa43f2ce61186dbe306465c
TheJebForge <jeb@thejebforge.com>
2022-12-31 20:46:16 +0200

    Added nbt example

--- data/products.example.lua
+++ data/products.example.lua
@@ -5,4 +5,11 @@ return {
     id = "minecraft:emerald",
     metaname = "mald",
   },
+  {
+    displayName = "Mending",
+    price = 1,
+    id = "minecraft:enchanted_book",
+    metaname = "mend",
+    nbt = "704a1bcdf9953c791651a77b1fe78891",
+  }
 }
